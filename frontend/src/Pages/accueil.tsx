394804c7d05feb6de362916054d5827b35015ef9
Whixon <cyril.houppertz@gmail.com>
2026-09-20 22:50:37 +0200

    Creating Home Page and first design

---
+++ frontend/src/Pages/accueil.tsx
@@ -0,0 +1,27 @@
+
+export function Accueil() {
+
+  return (
+    <>
+      <body className="accueil">
+
+        <div className="acmenu">
+          <table>
+            
+            <th>
+              <button className="projet">Projets </button>
+              
+              <button className="skills">Compétence </button>
+              
+              <button className="hire">M'embaucher ? </button>
+              
+              <button className="contact">Me contacter</button>
+            </th>
+          </table>
+        </div>
+
+        <h1 className="acTitle">Mon Portfolio</h1>
+      </body>
+    </>
+  )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Suspense } from 'react';
+
 import './index.css'
+
 import App from './App.tsx'
+import { Accueil } from './Pages/accueil.tsx';
+
+const router = createBrowserRouter([
+  {
+    path:'',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Accueil/>
+      },
+      {
+        path: '/selection',
+        element: null
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading translations...</div>}>
+      <RouterProvider router ={router}/>
+    </Suspense>
   </StrictMode>,
 )
