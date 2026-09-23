ca8190bc25892605c970d9389bfdc6b63a4171fb
Whixon <92325023+Whlxon@users.noreply.github.com>
2026-09-23 17:16:21 +0200

    Add files via upload

---
+++ frontend/src/Pages/home/home.tsx
@@ -0,0 +1,16 @@
+
+
+export function Home() {
+  
+
+  return (
+    <>
+      <body className="home">
+
+        
+
+        <h1 className="acTitle">Mon Portfolio</h1>
+      </body>
+    </>
+  )
+}
