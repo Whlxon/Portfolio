c48d92a0f70fda5396c501d11df2b1820c5dd038
Whixon <92325023+Whlxon@users.noreply.github.com>
2026-09-23 17:13:23 +0200

    Add files via upload

---
+++ Portfolio/frontend/src/Pages/notAvailable/nAvailable.tsx
@@ -0,0 +1,14 @@
+
+
+export function NotAvailable() {
+    return(
+        <>
+            <body className="notAv">
+
+                <h1 className="notTitle">Tu es tomber dans un trou sans fin !</h1>
+                <h2 >naaaaan j'rigole </h2>
+                
+            </body>
+        </>
+    )
+}
